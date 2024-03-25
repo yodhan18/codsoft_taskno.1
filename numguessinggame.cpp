ad5631fcb1a2bec39e6d60713e8b9d06d4739a6c
yodhan18 <133799707+yodhan18@users.noreply.github.com>
2024-03-25 16:16:03 +0530

    Update numguessinggame.cpp

--- numguessinggame.cpp
+++ numguessinggame.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main()
 {
 	int num;
-      int guess;
-      int tries = 0;
+        int guess;
+        int tries = 0;
 	srand(time(0)); 
 	num = rand() % 100 + 1; // random number between 1 and 100
 	cout << "Guess My Number Game\n\n";
